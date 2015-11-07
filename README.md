ae6bd3c518d04869eb73480ddf851a8be1cdf3a6
jmb <jamie@whitepumpkin.com>
2015-11-07 01:06:54 -0500

    initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+blueharvest library
+
+
+tracert -h 100 216.81.59.173
+telnet towel.blinkenlights.nl
---
+++ src/main/java/blueharvest/geocaching/concepts/address.java
@@ -0,0 +1,73 @@
+package blueharvest.geocaching.concepts;
+
+/**
+ *
+ * @author jmb
+ * @since 2015-10-13
+ */
+public abstract class address {
+
+    private final String street;
+    private final String city;
+    private final region region;
+    private final String postalcode;
+
+    /**
+     * <h3>address constructor</h3>
+     *
+     * @param street the street of the address
+     * @param city the city of the address
+     * @param region the region of the address
+     * @param postalcode the postal code of the address
+     * @since 2015-10-13
+     */
+    public address(String street, String city, region region, String postalcode) {
+        this.street = street;
+        this.city = city;
+        this.region = region;
+        this.postalcode = postalcode;
+    }
+
+    public String getStreet() {
+        return street;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public region getRegion() {
+        return region;
+    }
+
+    public String getPostalCode() {
+        return postalcode;
+    }
+
+    public static class region {
+
+        private final String name;
+        private final String abbreviation;
+
+        /**
+         * <h3>region constructor</h3>
+         *
+         * @param name the name of the region
+         * @param abbreviation the abbreviation of the region
+         */
+        public region(String name, String abbreviation) {
+            this.name = name;
+            this.abbreviation = abbreviation;
+        }
+
+        public String getName() {
+            return name;
+        }
+
+        public String getAbbrevation() {
+            return abbreviation;
+        }
+
+    }
+
+}
